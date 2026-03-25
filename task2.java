cd02ec995c64bbd242da03af58b51963c8823ee0
sottokevin-cyber <sottokevin72@gmail.com>
2026-03-25 15:44:14 +0800

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,12 @@
+public class task2 {
+
+public static void main(String[] args) {
+    byte zero = 0;
+    short one = 1;
+    int three = 3;
+    float two  = (float) 2.0;
+    boolean isTrue = true;
+    char h = 'H';
+    System.out.println(""+h+three+one+one+zero+" "+"W"+zero+"r"+one+"d " + two +" "+ isTrue);
+}
+}
