043276c95883b3e9f41d4e721b76515d3d220a94
sottokevin-cyber <sottokevin72@gmail.com>
2026-03-25 15:22:57 +0800

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,6 +1,10 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class task1 {
 
-class helloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
-}
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    
+} }
