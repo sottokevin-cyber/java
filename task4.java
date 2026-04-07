0eed2b2a8a60619a61692db5d660d63780b9ed1d
sottokevin-cyber <sottokevin72@gmail.com>
2026-04-07 11:59:31 +0800

    Update task4.java

--- task4.java
+++ task4.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class task4{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        String input = scanner.nextLine();
+        StringBuilder palindrome = new StringBuilder(input);
+        if (input.equals((palindrome.reverse()).toString())){
+            System.out.println("the input string is a palindrome");
+        }else{
+            System.out.println("the input string is not a palindrome");
+        }
+
+    }
+
+}
